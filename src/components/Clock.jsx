eff326d50bb9c0791b73cbfe05c8d478fac10128
brett-dazn <brett.jackson@dazn.com>
2022-11-18 19:23:36 +0000

    try

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 import dayjs from 'dayjs'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+dayjs.extend(advancedFormat)
 
 const Clock = () => {
     const [time, setTime] = useState(dayjs());
     const interval = setInterval(() => setTime(dayjs()), 1000)
     useEffect(() => () => clearInterval(interval), [])
 
-    const formatted = time.format('dddd DD MMMM YYYY, HH.mm')
+    const formatted = time.format('dddd Do MMMM YYYY, HH:mm')
 
     return formatted.substring(0, formatted.length - 3)
 }
