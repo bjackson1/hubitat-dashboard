704b04e3b21dfd94dad0a4af3ea82d83008b8f19
brett-dazn <brett.jackson@dazn.com>
2022-11-18 19:21:51 +0000

    try

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
 
 const Clock = () => {
-    const [time, setTime] = useState(Date.now());
-    const interval = setInterval(() => setTime(Date.now()), 1000)
+    const [time, setTime] = useState(dayjs());
+    const interval = setInterval(() => setTime(dayjs()), 1000)
     useEffect(() => () => clearInterval(interval), [])
 
-    const formatted = (new Date(time)).toLocaleString()
+    const formatted = time.format('dddd DD MMMM YYYY, HH.mm')
 
     return formatted.substring(0, formatted.length - 3)
 }
