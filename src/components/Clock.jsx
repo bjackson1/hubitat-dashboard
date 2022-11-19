305100f92340fad1166e32ac03b481aa477125ed
brett-dazn <brett.jackson@dazn.com>
2022-11-19 12:11:10 +0000

    try

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -4,11 +4,11 @@ import advancedFormat from 'dayjs/plugin/advancedFormat'
 dayjs.extend(advancedFormat)
 
 const Clock = () => {
-    const [time, setTime] = useState(dayjs());
-    const interval = setInterval(() => setTime(dayjs()), 1000)
+    const [time, setTime] = useState('no time');
+    const interval = setInterval(() => setTime(dayjs().format('dddd Do MMMM YYYY, HH:mm')), 5000)
     useEffect(() => () => clearInterval(interval), [])
 
-    return time.format('dddd Do MMMM YYYY, HH.mm')
+    return time
 }
 
 export default Clock
